3e8cd83bfb1173662f554c674e456feb91ff2986
GupilChhabs <133694101+GupilChhabs@users.noreply.github.com>
2026-01-22 04:04:05 +0530

    Create 4_complement_of_complement.lean

---
+++ 2_complement_world/4_complement_of_complement.lean
@@ -0,0 +1,11 @@
+apply Subset.antisymm
+intro x h2
+rewrite [mem_compl_iff] at h2
+rewrite [mem_compl_iff] at h2
+push_neg at h2
+exact h2
+intro x h2
+rewrite [mem_compl_iff]
+rewrite [mem_compl_iff]
+push_neg
+exact h2
